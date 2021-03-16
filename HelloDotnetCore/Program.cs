4a7638a8a6f57c4f611a37c6dafed959c760b199
shixiaoyong-tw <80743736+shixiaoyong-tw@users.noreply.github.com>
2021-03-16 17:23:53 +0800

    Add files via upload

---
+++ HelloDotnetCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloDotnetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dotnet core");
+            Console.ReadLine();
+        }
+    }
+}
